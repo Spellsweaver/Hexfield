7219d09dcc91fdbdec5a16be3fab792a1362a648
Spellweaver <spellsweaver@gmail.com>
2019-03-28 20:37:24 +0300

    Allow extra files upload

--- models/graphicsModel.lua
+++ models/graphicsModel.lua
@@ -10,6 +10,12 @@ function model.preload()
 			model[item][i]=love.graphics.newImage(item.."s/"..i..".png")		
 			i=i+1
 		end
+		local internal_items_num = i-1
+		i=1
+		while love.filesystem.getInfo("extra/"..item.."s/"..i..".png") do
+			model[item][i+internal_items_num]=love.graphics.newImage("extra/"..item.."s/"..i..".png")		
+			i=i+1
+		end
 	end
 end
 
